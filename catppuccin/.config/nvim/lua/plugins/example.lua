cb54b59a882f323f60f1be6ddb9fac6d50bd5a22
cristhian <cristhianfernandolp@gmail.com>
2025-11-06 22:11:57 -0300

    restart

---
+++ catppuccin/.config/nvim/lua/plugins/example.lua
@@ -0,0 +1,142 @@
+return {}
+-- return {
+--
+--   -- change trouble config
+--   {
+--     "folke/trouble.nvim",
+--     -- opts will be merged with the parent spec
+--     opts = { use_diagnostic_signs = true },
+--   },
+--
+--   -- disable trouble
+--   { "folke/trouble.nvim", enabled = false },
+--
+--   -- override nvim-cmp and add cmp-emoji
+--   {
+--     "hrsh7th/nvim-cmp",
+--     dependencies = { "hrsh7th/cmp-emoji" },
+--     ---@param opts cmp.ConfigSchema
+--     opts = function(_, opts)
+--       table.insert(opts.sources, { name = "emoji" })
+--     end,
+--   },
+--
+--   -- change some telescope options and a keymap to browse plugin files
+--   {
+--     "nvim-telescope/telescope.nvim",
+--     keys = {
+--       -- add a keymap to browse plugin files
+--       -- stylua: ignore
+--       {
+--         "<leader>fp",
+--         function() require("telescope.builtin").find_files({ cwd = require("lazy.core.config").options.root }) end,
+--         desc = "Find Plugin File",
+--       },
+--     },
+--     -- change some options
+--     opts = {
+--       defaults = {
+--         layout_strategy = "horizontal",
+--         layout_config = { prompt_position = "top" },
+--         sorting_strategy = "ascending",
+--         winblend = 0,
+--       },
+--     },
+--   },
+--
+--   -- add tsserver and setup with typescript.nvim instead of lspconfig
+--   {
+--     "neovim/nvim-lspconfig",
+--     dependencies = {
+--       "jose-elias-alvarez/typescript.nvim",
+--       init = function()
+--         require("lazyvim.util").lsp.on_attach(function(_, buffer)
+--           -- stylua: ignore
+--           vim.keymap.set( "n", "<leader>co", "TypescriptOrganizeImports", { buffer = buffer, desc = "Organize Imports" })
+--           vim.keymap.set("n", "<leader>cR", "TypescriptRenameFile", { desc = "Rename File", buffer = buffer })
+--         end)
+--       end,
+--     },
+--     ---@class PluginLspOpts
+--     opts = {
+--       ---@type lspconfig.options
+--       servers = {
+--         -- tsserver will be automatically installed with mason and loaded with lspconfig
+--         tsserver = {},
+--       },
+--       -- you can do any additional lsp server setup here
+--       -- return true if you don't want this server to be setup with lspconfig
+--       ---@type table<string, fun(server:string, opts:_.lspconfig.options):boolean?>
+--       setup = {
+--         -- example to setup with typescript.nvim
+--         tsserver = function(_, opts)
+--           require("typescript").setup({ server = opts })
+--           return true
+--         end,
+--         -- Specify * to use this function as a fallback for any server
+--         -- ["*"] = function(server, opts) end,
+--       },
+--     },
+--   },
+--
+--   -- for typescript, LazyVim also includes extra specs to properly setup lspconfig,
+--   -- treesitter, mason and typescript.nvim. So instead of the above, you can use:
+--   { import = "lazyvim.plugins.extras.lang.typescript" },
+--
+--   -- add more treesitter parsers
+--   {
+--     "nvim-treesitter/nvim-treesitter",
+--     opts = {
+--       ensure_installed = {
+--         "bash",
+--         "html",
+--         "javascript",
+--         "json",
+--         "lua",
+--         "markdown",
+--         "markdown_inline",
+--         "python",
+--         "query",
+--         "regex",
+--         "tsx",
+--         "typescript",
+--         "vim",
+--         "yaml",
+--       },
+--     },
+--   },
+--
+--   -- since `vim.tbl_deep_extend`, can only merge tables and not lists, the code above
+--   -- would overwrite `ensure_installed` with the new value.
+--   -- If you'd rather extend the default config, use the code below instead:
+--   {
+--     "nvim-treesitter/nvim-treesitter",
+--     opts = function(_, opts)
+--       -- add tsx and treesitter
+--       vim.list_extend(opts.ensure_installed, {
+--         "tsx",
+--         "typescript",
+--       })
+--     end,
+--   },
+--
+--   -- nvim-neoo-tree custom settings to not hide dotfiles
+--   {
+--     "nvim-neo-tree/neo-tree.nvim",
+--     branch = "v3.x",
+--     dependencies = {
+--       "nvim-lua/plenary.nvim",
+--       "MunifTanjim/nui.nvim",
+--       "nvim-tree/nvim-web-devicons",
+--     },
+--     lazy = false,
+--     filesystem = {
+--       filtered_items = {
+--         hide_dotfiles = false,
+--       },
+--     },
+--   },
+--
+--   -- add jsonls and schemastore packages, and setup treesitter for json, json5 and jsonc
+--   { import = "lazyvim.plugins.extras.lang.json" },
+-- }
---
+++ monochrome/config/nvim/lua/plugins/colorschemes.lua
@@ -0,0 +1,9 @@
+return {
+  {
+    "datsfilipe/vesper.nvim",
+    name = "vesper",
+    config = function()
+      vim.cmd.colorscheme("vesper")
+    end,
+  },
+}
