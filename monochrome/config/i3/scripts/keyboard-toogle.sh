7c11cc61d86c9df3dba80834c41404f7ac8b451f
cristhian <cristhianfernandolp@gmail.com>
2025-11-19 09:39:16 -0300

    remove old monochrome theme
    I'll make a new one ASAP

--- monochrome/config/i3/scripts/keyboard-toogle.sh
+++
@@ -1,3 +0,0 @@
-setxkbmap -layout "us,us" -variant ",intl" -option "grp:alt_shift_toggle"
-
-chmod +x ~/.config/i3/scripts/keyboard-toogle.sh
--- monochrome/config/nvim/lua/plugins/colorschemes.lua
+++
@@ -1,9 +0,0 @@
-return {
-  {
-    "datsfilipe/vesper.nvim",
-    name = "vesper",
-    config = function()
-      vim.cmd.colorscheme("vesper")
-    end,
-  },
-}
